bbfc01ff1f085366e7eeaf4dc90e243666a763f8
omerfuyar <denemeyo@gmail.com>
2026-04-08 00:47:54 +0300

    -gitignore -header file

---
+++ shumes.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#pragma region Shucio Macros
+
+#pragma endregion Shucio Macros
+
+#pragma region Shucio Declarations
+
+#pragma endregion Shucio Declarations
+
+#pragma region Shucio Definitions
+
+#pragma endregion Shucio Definitions
